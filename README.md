ccd5bbe338424191921230c0b08d678c49b5f9d0
Isa Inalcik <isa.inalcik@gmail.com>
2025-04-06 15:12:15 +0200

    add new features

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 Iceberg Metadata Insights is a Streamlit-based application designed to provide comprehensive insights into Apache Iceberg table metadata. It allows users to analyze, optimize, and explore Iceberg table statistics, snapshots, file details, and more.
 
 # Demo
-See demo video on LinkedIn: [Iceberg Metadata Insights Demo](https://www.linkedin.com/posts/isainalcik_opensource-apacheiceberg-trino-activity-7313174195268444160-Dn3L?utm_source=share&utm_medium=member_desktop&rcm=ACoAAAy1qYIBiVCMD34GF_PLP6AL-kOpMZ8AWCA)
 
+See demo video on LinkedIn: [Iceberg Metadata Insights Demo](https://www.linkedin.com/posts/isainalcik_opensource-apacheiceberg-trino-activity-7313174195268444160-Dn3L?utm_source=share&utm_medium=member_desktop&rcm=ACoAAAy1qYIBiVCMD34GF_PLP6AL-kOpMZ8AWCA)
 
 [![LinkedIn Post](./demo/screenshot.png)](https://www.linkedin.com/posts/isainalcik_opensource-apacheiceberg-trino-activity-7313174195268444160-Dn3L?utm_source=share&utm_medium=member_desktop&rcm=ACoAAAy1qYIBiVCMD34GF_PLP6AL-kOpMZ8AWCA)
 
@@ -30,14 +30,46 @@ See demo video on LinkedIn: [Iceberg Metadata Insights Demo](https://www.linkedi
   - Hive Metastore
   - Existing iceberg tables
 
+## Installation
+
+Create .env file in the root directory and add your Trino connection details:
+
+```bash
+TRINO_HOST=your_trino_host
+TRINO_PORT=your_trino_port
+TRINO_USER=your_trino_user
+TRINO_CATALOG=your_trino_catalog
+TRINO_SCHEMA=your_trino_schema
+TRINO_HTTP_SCHEME=http
+```
+
+## Docker
+
+To run the application in a Docker container, you can use the provided Dockerfile. This allows for easy deployment and isolation of dependencies.
+
+### Build the Docker Image
+
+```bash
+docker build -t iceberg-metadata-insights .
+```
+
+### Run the Docker Container
+
+```bash
+docker run -p 8501:8501 -e TRINO_HOST=your_trino_host -e TRINO_PORT=your_trino_port -e TRINO_USER=your_trino_user iceberg-metadata-insights
+```
+
 ## How to Run
 
 1. Clone the repository:
+
    ```bash
    git clone https://github.com/alaturqua/iceberg-metadata-insights.git
    cd iceberg-metadata-insights
    ```
+
    Install the required Python libraries:
+
    ```bash
    python -m venv .venv
    source .venv/bin/activate  # On Windows use `.venv\Scripts\activate`
@@ -45,6 +77,7 @@ See demo video on LinkedIn: [Iceberg Metadata Insights Demo](https://www.linkedi
    ```
 
 2. Start the Streamlit app:
+
    ```bash
    streamlit run ./src/app.py
    ```
@@ -58,7 +91,6 @@ See demo video on LinkedIn: [Iceberg Metadata Insights Demo](https://www.linkedi
 3. Perform table actions like optimization, snapshot expiration, and more.
 4. Explore detailed metadata through various tabs.
 
-
 ## License
 
 This project is licensed under the GNU GPLv3 License. See the [LICENSE](LICENSE) file for details.
@@ -70,4 +102,5 @@ This project is licensed under the GNU GPLv3 License. See the [LICENSE](LICENSE)
 - [Trino](https://trino.io/)
 
 ## Contributing
+
 Contributions are welcome! If you have suggestions or improvements, please open an issue or submit a pull request.
--- src/app.py
+++ src/app.py
@@ -6,13 +6,17 @@ import pandas as pd
 import plotly.express as px
 from streamlit_extras.metric_cards import style_metric_cards
 from streamlit_extras.theme import st_theme
+from ydata_profiling import ProfileReport
+from streamlit_ydata_profiling import st_profile_report
 
 from utils.connection import (
     init_connection,
-    fetch_stats,
-    load_snapshot_history,
-    load_file_details,
     execute_alter_table,
+    fetch_stats,
+    load_column_sizes,
+    load_daily_growth,
+    load_file_details,
+    load_snapshot_history,
 )
 
 st.set_page_config(
@@ -122,23 +126,116 @@ def main():
         else:
             style_metric_cards()
 
+        st.divider()
+        st.subheader("📈 Snapshots")
         snapshot_history = load_snapshot_history(cursor, schema, table)
-        st.subheader("⏳ Snapshot Timeline")
-        if not snapshot_history.empty:
-            fig_snapshots = px.scatter(
-                snapshot_history,
-                x="Committed At",
-                y="Operation",
-                color="Operation",
-                hover_data=["Snapshot ID", "Parent ID", "Summary"],
-                title="Snapshot Timeline",
-            )
-            st.plotly_chart(fig_snapshots, use_container_width=True)
-            with st.expander("Snapshot Details"):
-                st.dataframe(snapshot_history)
-        else:
-            st.info("No snapshot history available.")
+        snapshots_tab = st.tabs(
+            [
+                "📸 Snapshot Timeline",
+                "📈 Growth Row Metrics",
+                "📈 Growth File Metrics",
+            ]
+        )
+        with snapshots_tab[0]:
+            st.subheader("⏳ Snapshot Timeline")
+            if not snapshot_history.empty:
+                fig_snapshots = px.scatter(
+                    snapshot_history,
+                    x="Committed At",
+                    y="Operation",
+                    color="Operation",
+                    hover_data=["Snapshot ID", "Parent ID", "Summary"],
+                    title="Snapshot Timeline",
+                )
+                st.plotly_chart(fig_snapshots, use_container_width=True)
+                with st.expander("Snapshot Details"):
+                    st.dataframe(snapshot_history)
+            else:
+                st.info("No snapshot history available.")
+        with snapshots_tab[1]:
+            st.subheader("📈 Growth Metrics")
+            daily_growth = load_daily_growth(cursor, schema, table)
+            if not daily_growth.empty:
+                fig_growth = px.scatter(
+                    daily_growth,
+                    x="Committed At",
+                    y=["Added Rows Count", "Deleted Rows Count"],
+                    title="Daily Growth Metrics",
+                    labels={"value": "Count", "variable": "Metric"},
+                )
+                st.plotly_chart(fig_growth, use_container_width=True)
+                with st.expander("Detailed Daily Growth Data"):
+                    st.dataframe(daily_growth)
+            else:
+                st.info("No daily growth data available.")
+        with snapshots_tab[2]:
+            st.subheader("📈 Growth File Metrics")
+            daily_growth_files = load_daily_growth(cursor, schema, table)
+            if not daily_growth_files.empty:
+                fig_growth_files = px.scatter(
+                    daily_growth_files,
+                    x="Committed At",
+                    y=["Added Data Files Count", "Deleted Data Files Count"],
+                    title="Daily Growth File Metrics",
+                    labels={"value": "Count", "variable": "Metric"},
+                )
+                st.plotly_chart(fig_growth_files, use_container_width=True)
 
+                with st.expander("Detailed Daily Growth File Data"):
+                    daily_growth_files["Committed At"] = pd.to_datetime(
+                        daily_growth_files["Committed At"]
+                    ).dt.date
+
+                    detail_tabs = st.tabs(
+                        ["Raw Data", "Added Files by Day", "Deleted Files by Day"]
+                    )
+
+                    with detail_tabs[0]:
+                        st.dataframe(daily_growth_files)
+
+                    with detail_tabs[1]:
+                        st.dataframe(
+                            daily_growth_files[
+                                ["Committed At", "Added Data Files Count"]
+                            ]
+                            .groupby("Committed At")
+                            .sum()
+                        )
+
+                    with detail_tabs[2]:
+                        st.dataframe(
+                            daily_growth_files[
+                                ["Committed At", "Deleted Data Files Count"]
+                            ]
+                            .groupby("Committed At")
+                            .sum()
+                        )
+            else:
+                st.info("No daily growth file data available.")
+        st.subheader("📊 Column Sizes")
+        column_sizes = load_column_sizes(cursor, schema, table)
+        if not column_sizes.empty:
+            fig_column_sizes = px.bar(
+                column_sizes,
+                x="Column Name",
+                y="Size (Bytes)",
+                title="Column Sizes",
+                color="Data Type",
+                text="Size (Bytes)",
+            )
+            fig_column_sizes.update_traces(
+                texttemplate="%{text:.2s}", textposition="outside"
+            )
+            fig_column_sizes.update_layout(
+                uniformtext_minsize=8, uniformtext_mode="hide"
+            )
+            st.plotly_chart(fig_column_sizes, use_container_width=True)
+            with st.expander("Detailed Column Sizes"):
+                st.dataframe(column_sizes)
+        else:
+            st.info("No column sizes available.")
+
+        st.divider()
         file_details = load_file_details(cursor, schema, table)
         st.subheader("📂 File Size Distribution")
         if not file_details.empty:
@@ -212,7 +309,20 @@ def main():
             try:
                 manifests_df = pd.DataFrame(
                     cursor.execute(
-                        f'SELECT * FROM {schema}."{table}$manifests"'
+                        f'''
+                        SELECT
+                            path,
+                            length,
+                            partition_spec_id,
+                            added_snapshot_id,
+                            added_data_files_count,
+                            added_rows_count,
+                            existing_data_files_count,
+                            existing_rows_count,
+                            deleted_data_files_count,
+                            deleted_rows_count   
+                        FROM {schema}."{table}$manifests"
+                        '''
                     ).fetchall(),
                     columns=[
                         "Path",
@@ -220,12 +330,11 @@ def main():
                         "Partition Spec ID",
                         "Added Snapshot ID",
                         "Added Data Files Count",
+                        "Added Rows Count",
                         "Existing Data Files Count",
+                        "Existing Rows Count",
                         "Deleted Data Files Count",
-                        "Added Position Deletes Count",
-                        "Existing Position Deletes Count",
-                        "Deleted Position Deletes Count",
-                        "Partitions",
+                        "Deleted Rows Count",
                     ],
                 )
                 st.dataframe(manifests_df, use_container_width=True)
@@ -235,7 +344,16 @@ def main():
             try:
                 all_manifests_df = pd.DataFrame(
                     cursor.execute(
-                        f'SELECT * FROM {schema}."{table}$all_manifests"'
+                        f'''
+                        SELECT 
+                            path,
+                            length,
+                            partition_spec_id,
+                            added_snapshot_id,
+                            added_data_files_count,
+                            existing_data_files_count,
+                            deleted_data_files_count
+                        FROM {schema}."{table}$all_manifests"'''
                     ).fetchall(),
                     columns=[
                         "Path",
@@ -245,7 +363,6 @@ def main():
                         "Added Data Files Count",
                         "Existing Data Files Count",
                         "Deleted Data Files Count",
-                        "Partition Summaries",
                     ],
                 )
                 st.dataframe(all_manifests_df, use_container_width=True)
@@ -294,6 +411,7 @@ def main():
                         f'SELECT * FROM {schema}."{table}$partitions"'
                     ).fetchall(),
                     columns=[
+                        "Partition",
                         "Record Count",
                         "File Count",
                         "Total Size",
@@ -373,6 +491,26 @@ def main():
                 st.dataframe(refs_df, use_container_width=True)
             except Exception as e:
                 st.error(f"Error loading references: {str(e)}")
+
+        st.divider()
+        st.subheader("📊 Data Profiling")
+        if st.button("📊 Generate Profile Report", use_container_width=True):
+            with st.spinner("Generating profile report..."):
+                try:
+                    query = f"SELECT * FROM {schema}.{table}"
+                    df = pd.read_sql(query, conn)
+                    pr = ProfileReport(
+                        df,
+                        title="Data Profiling Report",
+                        explorative=True,
+                        minimal=True,
+                    )
+                    st_profile_report(pr, navbar=True)
+                    st.dataframe(df)
+                    st.success("Profile report generated successfully.")
+
+                except Exception as e:
+                    st.error(f"Error generating profile report: {str(e)}")
 
 
 if __name__ == "__main__":
